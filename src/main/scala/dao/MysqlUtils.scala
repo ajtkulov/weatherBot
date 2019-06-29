20e4c3442ea925a8ad372b78441760aab355f436
Pavel Ajtkulov <ajtkulov@gmail.com>
2019-06-29 14:24:19 +0500

    -

--- src/main/scala/dao/MysqlUtils.scala
+++ src/main/scala/dao/MysqlUtils.scala
@@ -5,7 +5,7 @@ import slick.jdbc.MySQLProfile.api._
 
 object MysqlUtils {
   lazy val db = Database.forURL(
-    url = "jdbc:mysql://127.0.0.1/weather",
+    url = "jdbc:mysql://127.0.0.1/weather?characterEncoding=UTF-8",
     user = "root",
     keepAliveConnection = true,
     password = "password"
